ed1894083e2ad71fd61bad589c5444b3ea33fdbd
Airam Vieira <leonardo.airam12@gmail.com>
2020-02-07 13:34:30 -0300

    added sound to heart beat

---
+++ index.js
@@ -0,0 +1,15 @@
+function playOceanSound(){
+  document.getElementById('ocean').play();
+}
+
+function pauseOceanSound(){
+  document.getElementById('ocean').pause();
+}
+
+function playHeartBeaten(){
+  document.getElementById('heart').play();
+}
+
+function pauseHeartBeaten(){
+  document.getElementById('heart').pause();
+}
