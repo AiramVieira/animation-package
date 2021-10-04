9057464c9b882cb2108b598b2e6ead932d9267ec
AiramVieira <leonardo.airam12@gmail.com>
2021-10-04 18:48:58 -0300

    3d cube

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ function playHeartBeaten(){
 function pauseHeartBeaten(){
   document.getElementById('heart').pause();
 }
+
+function playRickAstley() {
+  document.getElementById('pepe').play();
+}
+function pauseRickAstley() {
+  document.getElementById('pepe').pause();
+}
+
+function play(playerName) 
+  document.getElementById(playerName).play();
+}
+
+function pause(playerName) {
+  document.getElementById(playerName).pause();
+}
